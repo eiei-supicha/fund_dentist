4d1c285466987c32aa6f6340923c6c338e13a49a
eiei-supicha <pailoeisudsud@gmail.com>
2025-12-12 16:19:24 +0700

    edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) { // <--- เช็คว่าเป็น production ไหม
+            URL::forceScheme('https');
+        }
         \Illuminate\Pagination\Paginator
             ::defaultView('vendor.pagination.default');
         \Illuminate\Pagination\Paginator
